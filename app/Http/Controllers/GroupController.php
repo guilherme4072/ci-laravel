e3a160993c963912e5161a75ebeb782b8722dd54
Guilherme Cordeiro <guilherme4072@gmail.com>
2021-01-28 20:31:55 -0300

    added ci on groupcontroller

--- app/Http/Controllers/GroupController.php
+++ app/Http/Controllers/GroupController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers;
 
 use App\Models\Group;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+use function PHPUnit\Framework\isEmpty;
 
 class GroupController extends Controller
 {
@@ -14,13 +17,59 @@ class GroupController extends Controller
 
     public function store(Request $request)
     {
-        $data = $request->all();
-        $data['cnpj'] = preg_replace('/\D/','', $data['cnpj']);
-        return Group::create($data);
-    }
+			$data = $this->filterData($request->all());
+			$validator = $this->validator($data);
+			if ($validator->fails()) {
+				return response()->json($validator->errors())->setStatusCode(400);
+			}
+			return Group::create($data);
+		}
+		
+		private function validator(array $data)
+		{
+			$rules = $this->getRules();
+			$messages = $this->getMessages();
+			return Validator::make($data, $rules, $messages);
+		}
 
-    public function filterParams($data)
+		private function getRules() {
+			return [
+				'name' => 'required|max:50',
+				'cnpj' => 'required|cnpj|unique:groups'
+			];
+		}
+
+		private function getMessages() {
+			return [
+				'required' => 'O campo é obrigatório!',
+				'cnpj' => 'CNPJ é inválido!',
+				'max:50' => 'Máximo de :max caracteres!',
+				'unique' => ':input já em uso!'
+			];
+ 		}
+
+		private function filterData($values)
     {
-        //proxima aula
+			$data = [];
+			if (!$values) {
+				return $data;
+			}
+
+			foreach ($values as $key => $value) {
+				if (is_string($value) && trim(strip_tags($value))) {
+					$data[$key] = trim(strip_tags($value));
+				}
+				if (is_array($value) && !isEmpty($value)) {
+					$data[$key] = $this->filterData($value);
+				}
+				if (is_int($value) || is_float($value) || is_bool($value)) {
+					$data[$key] = $value;
+				}
+				if (isset($data['cnpj'])) {
+					$data['cnpj'] = preg_replace('/\D/','', $data['cnpj']);
+				}
+			}
+
+			return $data;
     }
 }
--- tests/Feature/Controllers/GroupControllerTest.php
+++ tests/Feature/Controllers/GroupControllerTest.php
@@ -10,7 +10,7 @@ use Tests\TestCase;
 
 class GroupControllerTest extends TestCase
 {
-    //use DatabaseMigrations;
+    use DatabaseMigrations;
     /**
      * A basic feature test example.
      *
@@ -47,5 +47,38 @@ class GroupControllerTest extends TestCase
 
         $response = $this->post('/api/groups', $data);
         $response->assertStatus(201);
+        $response->assertJsonStructure([
+            'code',
+            'name',
+            'cnpj',
+            'type',
+            'active',
+            'created_at',
+            'updated_at'
+            ]);
+            $responseArray = json_decode($response->content(), true);
+            $this->assertArrayHasKey('id', $responseArray);
+            $this->assertEquals(1, $responseArray['id']);
+            foreach( $data as $key => $value) {
+              if ($key == 'cnpj') {
+                $value = preg_replace('/\D/','', $data['cnpj']);
+              }
+              $this->assertEquals($value, $responseArray[$key]);
+            }
+        }
+
+    public function testStoreMethodWithExpectedRequiredFieldValidation()
+    {
+      $data = [];
+      $response = $this -> post('/api/groups', $data);
+      $response -> assertStatus(400);
+      $response -> assertJsonStructure([
+        'name',
+        'cnpj'
+      ]);
+      $response->assertExactJson([
+        'name' => ['O campo é obrigatório!'],
+        'cnpj' => ['O campo é obrigatório!']
+      ]);
     }
 }
